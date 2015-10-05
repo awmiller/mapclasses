04ab8c72db7ca56e89b84058ac92b48a241170dc
awmiller <awmiller.andros@gmail.com>
2015-10-05 15:22:51 -0700

    apsd

---
+++ map_item.cpp
@@ -0,0 +1,46 @@
+/* 
+ * File:   MapItem.cpp
+ * Author: awmil_000
+ * 
+ * Created on October 3, 2015, 10:55 AM
+ */
+
+#include "map_item.h"
+
+MapItem::MapItem() {
+    type = 'E';
+}
+
+MapItem::MapItem(char initializer) {
+    type = initializer;
+}
+
+MapItem::MapItem(const MapItem& orig) {
+    type = orig.getType();
+}
+
+char MapItem::getType() const{
+    return type;
+}
+
+void MapItem::setType(char newType){
+    type = newType;
+}
+    
+std::ifstream& operator>>(std::ifstream& nifstr, MapItem& mapitem){
+    char incoming;
+    nifstr >> incoming;
+    while((incoming == ' ')||(incoming == '\r')||(incoming == '\n')){
+        nifstr >> incoming;
+    }
+
+    mapitem.setType(incoming);
+
+    return nifstr;
+
+}
+    
+std::ostream& operator<<(std::ostream& ostrizr, MapItem& mapitem){
+    ostrizr << mapitem.type;
+    return ostrizr;
+}
